ba2052928a5cc584b5e6ed01d03112b4c58eda66
MatheusMachad00 <matheus_m_machado@hotmail.com>
2022-05-18 16:14:01 -0300

    Fix: Footer in seats screen fixed

--- src/components/seats/index.jsx
+++ src/components/seats/index.jsx
@@ -2,36 +2,80 @@ import Footer from "../footer"
 import { useEffect, useState } from "react";
 import { useParams, Link, useNavigate } from "react-router-dom";
 import axios from "axios";
-
+import {
+    SubTitle, ContainerRow, Seat, Infos, Selected, Avaliable, Unavaliable, Circles,
+    TextInfos, GIF, InputBlock, FinalizeButton} from "./style"
 
 
 export default function Seats(){
     const { idSection } = useParams();
-    const [data, setData] = useState([]);
+    const [data, setData] = useState(false);
 
     useEffect(() => {
         const request = axios.get(`https://mock-api.driven.com.br/api/v5/cineflex/showtimes/${idSection}/seats`);
         request.then(answer => {
-            /* const { data } = answer; */
-            setData(answer.data)
-            console.log(data)
-            console.log(data.movie.title)
-
-
+            const { data } = answer;
+            setData(data)
+            console.log(data.seats)
         })
         request.catch(<p>Carregando...</p>);
     }, []);
 
 
-    if(data === []){
+    if(data === false){
         return (<p>Carregando...</p>)
     }else {
         return(
             <>
-            <p>Tela seats</p>
-                <Footer title={data.movie.title} date={"18/05"} hour={'data.name'} posterURL={'data.movie.posterURL'}/>
+            <SubTitle>Selecione o(s) assento(s)</SubTitle>
+                <ContainerRow>
+                    {/* {
+                        data.seats.map((seat, index) => {
+                            const { id, isAvaliable, name } = seat
+                            return (<Seat key={index} onClick={() => movieSeats(id, name)}>{name}</Seat>)
+                        })
+                    } */}
+                </ContainerRow>
+                <Infos>
+                    <Circles>
+                        <Selected></Selected>
+                        <Avaliable></Avaliable>
+                        <Unavaliable></Unavaliable>
+                    </Circles>
+                    <TextInfos>
+                        <p>Disponível</p>
+                        <p>Selecionado</p>
+                        <p>Indisponível</p>
+                    </TextInfos>
+                </Infos>
+
+                {/* <div>
+                    <form onSubmit={userLogin}>
+                        <InputBlock>
+                            <label >Nome do comprador:</label>
+                            <input 
+                            type="text" 
+                            placeholder="Digite seu nome" 
+                            equired
+                            onChange={e => setUserName(e.target.value)}
+                            value={userName} />
+                        </InputBlock>
+
+                        <InputBlock>
+                            <label >CPF do comprador:</label>
+                            <input 
+                            type="text" 
+                            placeholder="Digite seu cpf" 
+                            required
+                            onChange={e => setCPF(e.target.value)}
+                            value={CPF}/>
+                            
+                        </InputBlock>
+                        <FinalizeButton type="submit">Reservar assento(s)</FinalizeButton>
+                    </form>
+                </div> */}
+                <Footer title={data.movie.title} date={data.day.date} hour={data.name} posterURL={data.movie.posterURL}/>
             </>
-            
             )
     }
 }
---
+++ src/components/seats/style.js
@@ -0,0 +1,160 @@
+import styled from 'styled-components'
+
+export const SubTitle = styled.div`
+display: flex;
+justify-content: center;
+text-align: center;
+align-items: center;
+font-size: 24px;
+width: 100%;
+height: 110px;
+color: #293845;
+font-family: 'Roboto';
+margin-top: -15px;
+`;
+
+export const ContainerRow = styled.div`
+display: flex;
+gap: 8px;
+justify-content: center;
+flex-wrap: wrap;
+margin-top: -10px;
+`
+
+export const Seat = styled.div`
+/* background-color: #C3CFD9; */
+border: 1px solid #808F9D;
+box-sizing: border-box;
+border-radius: 12px;
+color: #000000;
+font-family: 'Roboto';
+font-style: normal;
+font-weight: 400;
+font-size: 11px;
+line-height: 13px;
+width: 26px;
+height: 26px;
+display: flex;
+justify-content: center;
+text-align: center;
+align-items: center;
+margin-bottom: 5px;
+.avaliable{
+    background-color: #C3CFD9;
+    border: 1px solid #7B8B99;
+}
+.unavaliable{
+    background-color: #FBE192;
+    border: 1px solid #F7C52B;
+}
+.selected{
+    background-color: #8DD7CF;
+    border: 1px solid #1AAE9E;
+}
+`
+
+export const Infos = styled.div`
+display: flex;
+flex-direction: column;
+margin-top: 10px;
+`
+
+export const Circles = styled.div`
+display: flex;
+justify-content: space-around;
+`
+
+export const TextInfos = styled.div`
+display: flex;
+justify-content: space-around;
+p{
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 13px;
+    line-height: 15px;
+    color: #4E5A65;
+}
+`
+
+export const Selected = styled.div`
+background-color: #8DD7CF;
+border: 1px solid #1AAE9E;
+box-sizing: border-box;
+border-radius: 12px;
+width: 26px;
+height: 26px;
+`
+
+export const Avaliable = styled.div`
+background-color: #C3CFD9;
+border: 1px solid #7B8B99;
+box-sizing: border-box;
+border-radius: 12px;
+width: 26px;
+height: 26px;
+`
+
+export const Unavaliable = styled.div`
+background-color: #FBE192;
+border: 1px solid #F7C52B;
+box-sizing: border-box;
+border-radius: 12px;
+width: 26px;
+height: 26px;
+`
+
+export const InfoDivs = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+text-align: center;
+`
+
+export const GIF = styled.img`
+margin-left: 45px;
+`
+
+export const InputBlock = styled.div`
+display: flex;
+flex-direction: column;
+/* align-items: stretch; */
+/* background-color: red; */
+/* margin-top: -10px;
+margin-left: 24px; */
+input{
+    background: #FFFFFF;
+    border: 1px solid #D5D5D5;
+    box-sizing: border-box;
+    border-radius: 3px;
+    height: 51px;
+}
+label{
+    display: block;
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 18px;
+    line-height: 21px;
+    color: #293845;
+    margin-top: 7px;
+}
+`
+
+export const FinalizeButton = styled.button`
+display: flex;
+justify-content: center;
+align-items: center;
+text-align: center;
+width: 225px;
+height: 42px;
+background-color: #E8833A;
+font-family: 'Roboto';
+font-style: normal;
+font-weight: 400;
+font-size: 18px;
+line-height: 21px;
+color: #FFFFFF;
+margin: 57px auto 0 auto;
+border-radius: 3px;
+` 
